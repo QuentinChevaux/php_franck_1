759623f80b77237ff3eed7070198c96724cb98ae
QuentinChevaux <quentin.chevaux@gmail.com>
2022-01-25 15:57:15 +0100

    PHP connect database

--- article.php
+++ article.php
@@ -2,34 +2,38 @@
 
     $base_de_donnee = new PDO('mysql:dbname=articles_php_cours;host=localhost;charset=UTF8', 'root', '');
 
-    $requete = $base_de_donnee -> prepare('SELECT * FROM articles');
+    $requete = $base_de_donnee -> prepare('SELECT * FROM articles WHERE id = ?'); // LE ? SERT A EMPECHER LES INJECTIONS SQL POUR PLUS DE SECURITE
 
-    $requete -> execute(); 
+    $requete -> execute([ $_GET['id'] ]); // LE GET ID SE MET ICI POUR ENVOYER L'ID VERS LE ?
 
-    $articles = $requete ->fetchAll();
+    $articles = $requete ->fetch();
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Page Article</title>
 
+    <link rel="stylesheet" href="./bootstrap.css">
+    <link rel="stylesheet" href="./index.css">
+    <script src="./bootstrap.min.js" defer></script>
 
 </head>
 
 <body>
     
-    <h1>Titre de l'Article</h1>
+    <h1><?= $articles['titre'] ?></h1>
     
-    <p>Description</p>
-    <p>Sous Description</p>
+    <h2><?= $articles['description'] ?></h2>
+    <h3><?= $articles['description_sous_categorie'] ?></h3>
     
-    <img src="" alt="image">
+    <img src="./images/<?= $articles['image'] ?>" alt="image">
     
-    <p>Contenu</p>
+    <p class="text-center"><?= $articles['texte'] ?></p>
 
 </body>
 </html>
